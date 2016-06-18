5e3045d23cf5831a6b00d6e2a9e38b23197f6b6a
JeffLD <lejeff_d@hotmail.fr>
2016-06-19 00:56:26 +0200

    [SCRIPT] - Init variables + pattern curl

--- curl_delete.sh
+++ curl_delete.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 DELETE_REQ='--request DELETE --write "%{http_code} %{url_effective}\\n" --silent'
+USER_CNX=--user admin:admin123
+OUTPUT_TRACE=--output /dev/null
+
+URL_OLD_RELEASES=http://nexus:50080/nexus/service/local/repositories/releases/content/com/myproject/toto
+
+curl $DELETE_REQ $USER_CNX $OUTPUT_TRACE $URL_OLD_RELEASES'myproject/'
+curl $DELETE_REQ $USER_CNX $OUTPUT_TRACE $URL_OLD_RELEASES'myproject-api/'
+curl $DELETE_REQ $USER_CNX $OUTPUT_TRACE $URL_OLD_RELEASES'myproject-batch/'
+
 
